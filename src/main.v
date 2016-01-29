478df414807a9f159efb4b1e263dcd99110836e7
Jesús Arroyo Torrens <jesus.arroyo@bq.com>
2016-01-29 13:41:52 +0100

    Using module name from main

--- src/main.v
+++ src/main.v
@@ -1,22 +1,22 @@
 // Generated verilog
 
-module andc(input in0, in1, output out0);
+module andx(input in0, in1, output out0);
 assign out0 = in0 & in1;
 endmodule
 
-module notc(input in0, output out0);
+module notx(input in0, output out0);
 assign out0 = ! in0;
 endmodule
 
 module main(input in0, in1, output out0);
 wire w0;
-and and0 (
-    .in0(0),
-    .in1(0),
+andx and0 (
+    .in0(in0),
+    .in1(in1),
     .out0(w0)
 );
-not not1 (
+notx not0 (
     .in0(w0),
-    .in1(out0)
+    .out0(out0)
 );
 endmodule
