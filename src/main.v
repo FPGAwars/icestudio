d0e9e96499f02c491aebd83767cda24c849d6c45
Jesús Arroyo Torrens <jesus.arroyo@bq.com>
2016-02-03 11:53:19 +0100

    Added style Scott McCarthy

--- src/main.v
+++ src/main.v
@@ -1,13 +1,4 @@
 // Generated verilog
 
-module driver1x(output o0);
-assign o0 = 1'b1;
-endmodule
-
-module main(output output11);
-wire w0;
-assign output11 = w0;
-driver1x driver110 (
-    .o0(w0)
-);
+module main();
 endmodule
