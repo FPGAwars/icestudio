11811c38520570a03b8a003804af010b090d2276
Jesús Arroyo Torrens <jesus.jkhlg@gmail.com>
2016-06-14 11:45:31 +0200

    Add load/save input management

--- app/scripts/controllers/menu.js
+++ app/scripts/controllers/menu.js
@@ -4,11 +4,39 @@ angular.module('icestudio')
   .controller('MenuCtrl', function ($scope) {
 
     $scope.load = function() {
-      console.log('load');
+      setTimeout(function() {
+        var ctrl = angular.element('#input-load');
+        ctrl.on('change', function(event) {
+          var file = event.target.files[0];
+          event.target.files.clear();
+          if (file) {
+            load(file.path);
+          }
+        });
+        ctrl.click();
+      }, 0);
     }
 
     $scope.save = function() {
-      console.log('save');
+      setTimeout(function() {
+        var ctrl = angular.element('#input-save');
+        ctrl.on('change', function(event) {
+          var file = event.target.files[0];
+          event.target.files.clear();
+          if (file) {
+            save(file.path);
+          }
+        });
+        ctrl.click();
+      }, 0);
+    }
+
+    function load(filepath) {
+      alert("Load " + filepath);
+    }
+
+    function save(filepath) {
+      alert("Save " + filepath);
     }
 
     $scope.build = function() {
