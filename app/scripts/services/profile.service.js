391d819f9c3da5550d393fbf264a613d925f9f9e
Jesús Arroyo Torrens <jesus.jkhlg@gmail.com>
2016-08-11 16:41:05 +0200

    Store profile in .icestudio dir

--- app/scripts/services/profile.service.js
+++ app/scripts/services/profile.service.js
@@ -1,31 +1,35 @@
 'use strict';
 
 angular.module('icestudio')
-    .service('profile', function(utils) {
+    .service('profile', function(nodeFs, nodePath, utils) {
 
-      this.profilePath = './profile.json'
+        const BASE_DIR = process.env.HOME || process.env.USERPROFILE;
+        const ICESTUDIO_DIR = nodePath.join(BASE_DIR, '.icestudio');
+        const PROFILE_PATH = nodePath.join(ICESTUDIO_DIR, 'profile.json');
 
-      this.data = {
-        'language': 'en'
+        this.data = {
+          'language': 'en'
+        }
+
+        this.load = function(callback) {
+          utils.readFile(PROFILE_PATH, (function(_this) {
+            return function(data) {
+              if (data) {
+                _this.data = data;
+              }
+              if (callback)
+                callback()
+              console.log('Profile loaded');
+            };
+          })(this));
+        }
+
+        this.save = function() {
+          if (!nodeFs.existsSync(ICESTUDIO_DIR))
+            nodeFs.mkdirSync(ICESTUDIO_DIR);
+          utils.saveFile(PROFILE_PATH, this.data, function() {
+            console.log('Profile saved');
+          }, true);
+        }
       }
-
-      this.load = function(callback) {
-        utils.readFile(this.profilePath, (function(_this) {
-          return function(data) {
-            if (data) {
-              _this.data = data;
-            }
-            if (callback)
-              callback()
-            console.log('Profile loaded');
-          };
-        })(this));
-      }
-
-      this.save = function() {
-        utils.saveFile(this.profilePath, this.data, function() {
-          console.log('Profile saved');
-        }, true);
-      }
-
-  });
+  );
