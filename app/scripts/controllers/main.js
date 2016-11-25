ac3656a64817af36e9db6d80279fe65715eece71
Jesús Arroyo Torrens <jesus.jkhlg@gmail.com>
2016-11-25 18:30:40 +0100

    Fix Alertify buttons translation problem

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,53 +3,16 @@
 angular.module('icestudio')
   .controller('MainCtrl', function($scope, gettextCatalog) {
 
-    alertify.defaults = {
-      // dialogs defaults
-      modal:true,
-      basic:false,
-      frameless:false,
-      movable:false,
-      moveBounded:false,
-      resizable:true,
-      closable:true,
-      closableByDimmer:true,
-      maximizable:true,
-      startMaximized:false,
-      pinnable:true,
-      pinned:true,
-      padding: true,
-      overflow:true,
-      maintainFocus:true,
-      transition:'fade', // [ 'slide', 'zoom', 'flipx', 'flipy', 'fade', 'pulse' ]
-      autoReset:true,
+    alertify.defaults.movable = false;
+    alertify.defaults.notifier.delay = 2;
+    alertify.defaults.glossary.title = 'Icestudio';
 
-      // notifier defaults
-      notifier:{
-        // auto-dismiss wait time (in seconds)
-        delay:2,
-        // default position
-        position:'bottom-right'
-      },
-
-      // language resources
-      glossary:{
-        // dialogs default title
-        title:'Icestudio',
-        // ok button text
-        ok: gettextCatalog.getString('OK'),
-        // cancel button text
-        cancel: gettextCatalog.getString('Cancel')
-      },
-
-      // theme settings
-      theme:{
-        // class name attached to prompt dialog input textbox.
-        input:'ajs-input',
-        // class name attached to ok button
-        ok:'ajs-ok',
-        // class name attached to cancel button
-        cancel:'ajs-cancel'
-      }
-    };
+    setTimeout(function() {
+      var ok = gettextCatalog.getString('OK');
+      var cancel = gettextCatalog.getString('Cancel');
+      alertify.set('alert', 'labels', { 'ok': ok, 'cancel': cancel });
+      alertify.set('prompt', 'labels', { 'ok': ok, 'cancel': cancel });
+      alertify.set('confirm', 'labels', { 'ok': ok, 'cancel': cancel });
+    }, 100);
 
   });
