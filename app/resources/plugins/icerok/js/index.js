3056f3ac62709b8c9ecbeb1a38cbe332c3296482
cavearr <carlos@magnitude.es>
2020-10-22 21:37:52 +0200

    Test Plugin skeleton functions

--- app/resources/plugins/icerok/js/index.js
+++ app/resources/plugins/icerok/js/index.js
@@ -134,13 +134,17 @@ function startCapture(button) {
   }
 }
 
+/*
 window.addEventListener("message", (event) => {
-  // Do we trust the sender of this message?  (might be
-  // different from what we originally opened, for example).
- console.log(event.data);
-  // event.source is popup
-  // event.data is "hi there yourself!  the secret response is: rheeeeet!"
-}, false);
+
+  if(typeof event.data !== 'undefined' && event.data !== null && event.data !== false ){
+
+    console.log(event.data);
+ 
+
+  }
+
+}, false);*/
 
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -187,6 +191,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
 });
 
+function onLoad(params){
+
+  console.log('Arrancando plugin',params);
+}
 
 function onClose() {
 
