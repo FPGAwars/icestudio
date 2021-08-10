a106e286c8164ee57d36ddd8facdf3641bda0a13
Cavearr <charliva@gmail.com>
2021-08-10 23:29:05 +0200

    Remove check fo new stable version

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,55 +1,48 @@
-'use strict';
+"use strict";
 
-angular.module('icestudio')
-  .controller('MainCtrl', function ($scope,
-    gettextCatalog, tools, utils) {
-
+angular
+  .module("icestudio")
+  .controller("MainCtrl", function ($scope, gettextCatalog, tools, utils) {
     alertify.defaults.movable = false;
     alertify.defaults.closable = false;
-    alertify.defaults.transition = 'fade';
+    alertify.defaults.transition = "fade";
     alertify.defaults.notifier.delay = 3;
 
     setTimeout(function () {
       var labels = {
-        'ok': gettextCatalog.getString('OK'),
-        'cancel': gettextCatalog.getString('Cancel')
+        ok: gettextCatalog.getString("OK"),
+        cancel: gettextCatalog.getString("Cancel"),
       };
-      alertify.set('alert', 'labels', labels);
-      alertify.set('prompt', 'labels', labels);
-      alertify.set('confirm', 'labels', labels);
+      alertify.set("alert", "labels", labels);
+      alertify.set("prompt", "labels", labels);
+      alertify.set("confirm", "labels", labels);
     }, 100);
 
     /* If in package.json appears development:{mode:true}*/
     /* activate development tools */
     tools.ifDevelopmentMode();
 
-
-    $(document).delegate('.action-open-url-external-browser', 'click', function (e) {
-      e.preventDefault();
-      utils.openUrlExternalBrowser($(this).prop('href'));
-      return false;
-    });
-
-    /* Functions that checks if new version is available */
-    setTimeout(function () {
-      tools.checkForNewVersion();
-    }, 30000);
-
+    $(document).delegate(
+      ".action-open-url-external-browser",
+      "click",
+      function (e) {
+        e.preventDefault();
+        utils.openUrlExternalBrowser($(this).prop("href"));
+        return false;
+      }
+    );
 
     /* Plugin menu*/
 
-
-
-    let icmBodyEl = $('body'),
-
-      icmOpenbtn = document.getElementById('icm-open-button'),
-      icmClosebtn = document.getElementById('icm-close-button'),
+    let icmBodyEl = $("body"),
+      icmOpenbtn = document.getElementById("icm-open-button"),
+      icmClosebtn = document.getElementById("icm-close-button"),
       icmIsOpen = false,
-      icmMorphEl = document.getElementById('icm-morph-shape'),
-      icmTempsnap = Snap(icmMorphEl.querySelector('svg')),
-      icmPath = icmTempsnap.select('path'),
-      icmInitialPath = icmPath.attr('d'),
-      icmPathOpen = icmMorphEl.getAttribute('data-morph-open'),
+      icmMorphEl = document.getElementById("icm-morph-shape"),
+      icmTempsnap = Snap(icmMorphEl.querySelector("svg")),
+      icmPath = icmTempsnap.select("path"),
+      icmInitialPath = icmPath.attr("d"),
+      icmPathOpen = icmMorphEl.getAttribute("data-morph-open"),
       icmIsAnimating = false;
 
     function icmToggleMenu() {
@@ -58,33 +51,38 @@ angular.module('icestudio')
       }
       icmIsAnimating = true;
       if (icmIsOpen) {
-        icmBodyEl.removeClass('icm-show-menu');
+        icmBodyEl.removeClass("icm-show-menu");
         // animate path
         setTimeout(function () {
           // reset path
-          icmPath.attr('d', icmInitialPath);
+          icmPath.attr("d", icmInitialPath);
           icmIsAnimating = false;
         }, 300);
       } else {
-        icmBodyEl.addClass('icm-show-menu');
+        icmBodyEl.addClass("icm-show-menu");
         // animate path
-        icmPath.animate({
-          'path': icmPathOpen
-        }, 400, mina.easeinout, function () {
-          icmIsAnimating = false;
-        });
+        icmPath.animate(
+          {
+            path: icmPathOpen,
+          },
+          400,
+          mina.easeinout,
+          function () {
+            icmIsAnimating = false;
+          }
+        );
       }
       icmIsOpen = !icmIsOpen;
     }
 
-    icmOpenbtn.addEventListener('click', icmToggleMenu);
+    icmOpenbtn.addEventListener("click", icmToggleMenu);
 
     if (icmClosebtn) {
-      icmClosebtn.addEventListener('click', icmToggleMenu);
+      icmClosebtn.addEventListener("click", icmToggleMenu);
     }
 
     tools.initializePluginManager(icmToggleMenu);
 
-
     /***************************** */
-});
+  });
+
