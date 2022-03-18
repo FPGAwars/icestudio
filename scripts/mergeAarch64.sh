0bfd7c3b35965f61ea888f74f96f19207032c507
obijuan <obijuan.cube@gmail.com>
2022-03-18 09:30:16 +0100

    Arm build: Script little simplification

--- scripts/mergeAarch64.sh
+++ scripts/mergeAarch64.sh
@@ -12,6 +12,12 @@ GREEN="\033[1;32m"
 NW_NAME=nwjs-v0.58.1-linux-arm64  # <-- CHANGE THIS if NW is UPGRADED! <----
 NW_NAME_TAR_GZ="$NW_NAME.tar.gz"
 
+#-- Folder where to uncompress the NW dist tarball
+DIST_TMP="dist/tmp/nwjsAarch64"
+
+#-- Create the folder, if it does not exist
+mkdir -p $DIST_TMP
+
 #-- Folder where the final NW binary files should be copied
 DESTINATION="dist/icestudio/aarch64"
 
@@ -26,33 +32,29 @@ NW_DIST_TAR_GZ="$NW_CACHE/nwjs.tar.gz"
 
 #-- The NW tarball is locate in the following path, once the
 #-- DIST tarball is unzipped
-NW_PATH="$NW_CACHE/usr/docker/dist/nwjs-chromium-ffmpeg-branding"  
+NW_PATH="$DIST_TMP/usr/docker/dist/nwjs-chromium-ffmpeg-branding"  
 
 #-- Src folder where the NW files have been unzipped
-NW_SRC_PATH="$NW_CACHE/$NW_NAME"
+NW_SRC_PATH="$DIST_TMP/$NW_NAME"
 
 #-- NW tarball with path
 NW_TARBALL="$NW_PATH/$NW_NAME_TAR_GZ"
 
-#-- If a previous NW tarball was unzipped, remove it 
-if [ -d "$NW_PATH" ]; then
-  echo "${RED}Clean cache folder:${NC}"
-  echo "${RED}  ${NW_PATH} ${NC}"
 
-  rm -rf "$NW_PATH"
-fi
 
 #-- Unzip the NW Dist Tarball into the cache folder
 echo "${BLUE}Unzipping the NW Dist tarball: ${NW_DIST_TAR_GZ}${NC}"
-tar xzf $NW_DIST_TAR_GZ -C $NW_CACHE
+tar xzf $NW_DIST_TAR_GZ -C $DIST_TMP
 
 #-- Unzip the NW tarball into the cache folder
 echo "${BLUE}unzipping the NW tarball: $NW_NAME_TAR_GZ${NC}"
-tar xzf "$NW_TARBALL" -C $NW_CACHE
+tar xzf "$NW_TARBALL" -C $DIST_TMP
 
 #-- Copy the NW files to the DESTINATION folder
 
 echo "${GREEN}Copying NW files...${NC}"
+echo "From: $NW_SRC_PATH/*"
+echo "To: $DESTINATION/"
 cp -R $NW_SRC_PATH/* $DESTINATION/
 
 #-- The icestudio executable file is called nw
