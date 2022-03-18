65363213237953dd804605d8687fa3b48baad8be
obijuan <obijuan.cube@gmail.com>
2022-03-18 16:27:38 +0100

    Arm build: Script simplified

--- scripts/mergeAarch64.sh
+++ scripts/mergeAarch64.sh
@@ -13,7 +13,6 @@ echo "* ARGUMENTS: $1"
 
 #-- The NW tarball name (not extension) is received as an argument
 NW_NAME=$1 
-NW_NAME_TAR_GZ="$NW_NAME.tar.gz"
 
 #-- Folder where to uncompress the NW dist tarball
 DIST_TMP="dist/tmp/nwjsAarch64"
@@ -21,20 +20,8 @@ DIST_TMP="dist/tmp/nwjsAarch64"
 #-- Folder where the final NW binary files should be copied
 DESTINATION="dist/icestudio/aarch64"
 
-#-- The NW tarball is locate in the following path, once the
-#-- DIST tarball is unzipped
-NW_PATH="$DIST_TMP/usr/docker/dist/nwjs-chromium-ffmpeg-branding"  
-
 #-- Src folder where the NW files have been unzipped
 NW_SRC_PATH="$DIST_TMP/$NW_NAME"
-
-#-- NW tarball with path
-NW_TARBALL="$NW_PATH/$NW_NAME_TAR_GZ"
-
-
-#-- Unzip the NW tarball into the cache folder
-#echo "${BLUE}unzipping the NW tarball: $NW_NAME_TAR_GZ${NC}"
-#tar xzf "$NW_TARBALL" -C $DIST_TMP
 
 #-- Copy the NW files to the DESTINATION folder
 
