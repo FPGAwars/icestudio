c47daa2658f6d7c922e656270b9afae3de1b45cb
obijuan <obijuan.cube@gmail.com>
2022-05-05 20:18:20 +0200

    update_translation.sh updated

--- scripts/update_translations.sh
+++ scripts/update_translations.sh
@@ -3,8 +3,14 @@
 #      Update the translation files         #
 #############################################
 
+#-- File for the Statistics
+STATS_FILE="statistics.txt"
+
 #-- Update the .po file of all the locale folders
 cd app/resources/locale
+
+#-- Create a blank file
+echo "" > $STATS_FILE
 
 #-- d is the locale directory
 for d in */ ; do
@@ -16,6 +22,12 @@ for d in */ ; do
 
   #-- Update all the .po files
   msgmerge $LOCALE/$LOCALE.po template.pot -o $LOCALE/$LOCALE.po
+
+  #-- Add the statistics
+  echo $LOCALE >> $STATS_FILE
+  msgfmt --statistics $LOCALE/$LOCALE.po 2>> $STATS_FILE
+  echo "" >> $STATS_FILE
+
 
 done
 
