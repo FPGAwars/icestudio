595af116efe9b4a5b077834e6a526f129a7ca903
jojo535275 <65027759+jojo535275@users.noreply.github.com>
2023-02-23 16:28:40 +0100

    CM position/size update
    CM position/size updated

--- app/resources/libs/Icestudio/GUI/Widgets/WafleUIWindow.js
+++ app/resources/libs/Icestudio/GUI/Widgets/WafleUIWindow.js
@@ -6,16 +6,16 @@ class WafleUIWindow{
         params.html ='';
         }
         if(typeof params.y === 'undefined'){
-        params.y ='100px';
+        params.y ='70px';
         }
-        if(typeof params.x === 'undefined'){
-        params.x ='500px';
-        }
-        if(typeof params.height === 'undefined'){
-        params.height ='450px';
-        }
+        if(typeof params.right === 'undefined'){
+        params.right  ='0px';                     //CM window sticked to the rigth side of icestudio window
+        }               
         if(typeof params.width === 'undefined'){
-        params.width ='180px';
+           params.width ="calc(7% + 150px)";      //adjust size of CM window with icestudio window size
+        }               
+        if(typeof params.height === 'undefined'){   
+          params.height ="calc(100% - 118px)"        
         }
 
         /*-- ¡¡ Be careful !!
@@ -23,14 +23,16 @@ class WafleUIWindow{
         -- For the future we could change by svg image for the icons.
         -- */
         this.winId=params.id;
-        let content=`<div class="ics-wm-window--topbar | ics-wm__is-draggable" data-dragcontainerid="#${params.id}"><div class="ics-wm-window--topbar--button | ics-wm-window__close" data-winid="#${params.id}">x</div><div class="ics-wm-window--topbar--button | ics-wm-window__minify | hidden">-</div></div>
-                     <div class="ics-wm-window--body"></div>
-                     <div class="ics-wm-window--bottombar"></div>
-                    `;
+        let content=
+        `<div class="ics-wm-window--topbar | ics-wm__is-draggable" data-dragcontainerid="#${params.id}">
+        <div class="ics-wm-window--topbar--button | ics-wm-window__close" data-winid="#${params.id}">x</div>
+        <div class="ics-wm-window--topbar--button | ics-wm-window__minify | hidden">-</div></div>
+        <div class="ics-wm-window--body"></div>
+        <div class="ics-wm-window--bottombar"></div>`;
         
         this.dom = iceStudio.gui.addDiv(params.id,params.htmlClass,content);
         this.dom.style.top=params.y;
-        this.dom.style.left=params.x;
+        this.dom.style.right=params.right;
         this.dom.style.width=params.width;
         this.dom.style.height=params.height;
     }
